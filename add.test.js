b6066b0c31cc27b22a156c0dda498709ddad69b2
gupta6 <kgupta.gupta1996@gmail.com>
2024-12-05 18:49:36 +0530

    added second test case

--- add.test.js
+++ add.test.js
@@ -4,4 +4,7 @@ describe('add function', () => {
     it('should return 0 if input is empty', () => {
         expect(add('')).toBe(0);
     });
+    it('should return the number if only single number is passed', () => {
+        expect(add('2')).toBe(2);
+    });
 })
