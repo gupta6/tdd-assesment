5616aca42f4a468a8490969544ecdac501a2e0cc
gupta6 <kgupta.gupta1996@gmail.com>
2024-12-05 19:11:10 +0530

    added third test case

--- add.test.js
+++ add.test.js
@@ -4,7 +4,12 @@ describe('add function', () => {
     it('should return 0 if input is empty', () => {
         expect(add('')).toBe(0);
     });
+
     it('should return the number if only single number is passed', () => {
         expect(add('2')).toBe(2);
+    });
+
+    it('should return the addition of numbers seperated by comma in input string', () => {
+        expect(add('2,3,4')).toBe(9);
     });
 })
