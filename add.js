8cef835f18d8c9583d6773e39d5017eaa9c44bdc
gupta6 <kgupta.gupta1996@gmail.com>
2024-12-05 19:13:53 +0530

    updated code to handle third test case

--- add.js
+++ add.js
@@ -3,7 +3,13 @@ function add(inputString){
         return 0;
     }
 
-    return +inputString;
+    const nums = inputString.split(",");
+
+    if(nums.length === 1){
+        return +nums[0];
+    }
+    
+    return nums.reduce((acc, a) => acc+(+a), 0);
 }
 
 module.exports = add;
