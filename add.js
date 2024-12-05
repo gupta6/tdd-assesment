31c7385cd13f6663e5541b1c1fc452c8b1f130bd
gupta6 <kgupta.gupta1996@gmail.com>
2024-12-05 19:54:13 +0530

    updated code to handle fifth test case

--- add.js
+++ add.js
@@ -3,7 +3,16 @@ function add(inputString){
         return 0;
     }
 
-    const nums = inputString.split(/[\n,]/);
+    let delimiter = '';
+    let input = inputString;
+
+    if(inputString[0] === '/' && inputString[1] === '/'){
+        delimiter = inputString[2];
+        input = inputString.substring(3);
+    }
+
+    const splitter = new RegExp(`[,\n${delimiter}]`, 'g');
+    const nums = input.split(splitter);
 
     if(nums.length === 1){
         return +nums[0];
